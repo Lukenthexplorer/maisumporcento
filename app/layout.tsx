4570775b1a098b07e7eb11b2fb300fec8c63cb15
Lucas Kenji <lucas.kenji2002@outlook.com>
2026-02-08 13:47:52 -0300

    feat: Add Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { UserProvider } from '@/contexts/UserContext'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <UserProvider>
           {children}
         </UserProvider>
+        <Analytics />
       </body>
     </html>
   )
